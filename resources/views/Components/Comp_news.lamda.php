0bf29fd0c1a3178e41ecc96269013cc6fe6eb005
rizky <muhammatsaria026@student.unsrat.ac.id>
2026-01-26 16:33:51 +0800

    make clean

--- resources/views/Components/Comp_news.lamda.php
+++
@@ -1,9 +0,0 @@
- @foreach($news as $item)
-    <div class="bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl hover:scale-105 transition duration-300">
-        <div class="p-6">
-            <h2 class="text-xl font-semibold mb-2">{{ $item['title'] }}</h2>
-            <p class="text-gray-700 mb-4">{{ $item['content'] }}</p>
-            <a href="/news/{{ $item['slug'] }}" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">Baca Selengkapnya</a>
-        </div>
-    </div>
-@endforeach
--- routes/web.php
+++ routes/web.php
@@ -3,32 +3,14 @@
 
 use Lamda\Core\SSE\EventController;
 use Lamda\Core\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\NewsController;
-use App\Http\Controllers\DashboardController;
-
-use App\Http\Middlewares\AuthMiddleware;
-use App\Http\Middlewares\AfterAuthMiddleware;
 
 
-Route::get('/', [NewsController::class, 'index'], AuthMiddleware::class);
-Route::get('/news/{slug}', [NewsController::class, 'show'], AuthMiddleware::class);
-Route::get('/about', [NewsController::class, 'about']);
-Route::get('/contact', [NewsController::class, 'contact']);
 
-// Auth routes
-Route::get('/login', [AuthController::class, 'login'], AfterAuthMiddleware::class);
-Route::post('/login', [AuthController::class, 'loginPost'], AfterAuthMiddleware::class);
-Route::get('/register', [AuthController::class, 'register'], AfterAuthMiddleware::class);
-Route::post('/register', [AuthController::class, 'store'], AfterAuthMiddleware::class);
-Route::get('/logout', [AuthController::class, 'logout'], AuthMiddleware::class);
+// Register Web Routes Here
 
-// Dashboard route
-Route::get('/dashboard', [DashboardController::class, 'dashboard'], AuthMiddleware::class);
-Route::post('/dashboard', [DashboardController::class, 'store'], AuthMiddleware::class);
-Route::get('/dashboard/delete/{id}', [DashboardController::class, 'delete'], AuthMiddleware::class);
-Route::get('/dashboard/update/{id}', [DashboardController::class, 'update'], AuthMiddleware::class);
-Route::post('/dashboard/update/{id}', [DashboardController::class, 'updatePost'], AuthMiddleware::class);
+
+
+
 
 // Realtime routes
 Route::get('/events', [EventController::class, 'stream']);
